166debc83b39d152b7e675f643221bb591a148d9
ingyoe <melon7256@naver.com>
2022-01-18 22:01:11 +0900

    html.js

---
+++ raspberry/15_motor.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO 
+import time
+servo_pin =18 
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(servo_pin, GPIO.OUT)
+pwm = GPIO.PWM(servo_pin, 50)
+pwm.start(3.0)
+for t_high in range(30,125): 
+    pwm.ChangeDutyCycle(t_high/10.0)
+    time.sleep(0.02)
+pwm.ChangeDutyCycle(3.0)
+time.sleep(1.0)
+pwm.ChangeDutyCycle(0.0)
+pwm.stop() 
+GPIO.cleanup()
